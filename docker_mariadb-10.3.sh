3e8c819c4d471a9ba02c6f8c715ef5208f9c17d1
Nikola Koevski <nikola.koevski@camunda.com>
2019-05-21 17:01:20 +0200

    fix(script): remove wrong env vars

--- docker_mariadb-10.3.sh
+++ docker_mariadb-10.3.sh
@@ -3,7 +3,6 @@
 docker run -it --rm \
 	--name mariadb103 \
 	-p 127.0.0.1:3306:3306 \
-	-e TZ='America/Chicago' \
   	-e MYSQL_ROOT_PASSWORD=camunda \
   	-e MYSQL_USER=camunda \
   	-e MYSQL_PASSWORD=camunda \
--- docker_postgres-11.2.sh
+++ docker_postgres-11.2.sh
@@ -3,8 +3,7 @@
 docker run -i \
 	--name postgres112 \
 	-p 127.0.0.1:5432:5432 \
-  	-e POSTGRES_PASSWORD=camunda \
   	-e POSTGRES_USER=camunda \
-  	-e MYSQL_PASSWORD=camunda \
+  	-e POSTGRES_PASSWORD=camunda \
   	-e POSTGRES_DB=process-engine \
   	-t postgres:11.2
